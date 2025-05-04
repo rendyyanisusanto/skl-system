cebafbeadecb6001d6d5d4d9b74727a87f21eebf
Rendy <rendy@gmail.com>
2025-05-04 23:22:51 +0700

    Update vite.config.js -> allowed host

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: '0.0.0.0',  // agar bisa diakses dari IP LAN
-    port: 5522         // port yang kamu inginkan
+    host: '0.0.0.0',
+    port: 5522,
+    allowedHosts: ['sklsystem.simsmk.sch.id']
   }
 })
