da2becce748d8834e0e512d7230593e1e7e9a80e
Rendy <rendy@gmail.com>
2025-05-04 23:20:48 +0700

    Update package json -> port and hostname

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',  // agar bisa diakses dari IP LAN
+    port: 5522         // port yang kamu inginkan
+  }
 })
